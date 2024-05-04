336f39891ff77f7bc587e15b33bf83efc79e1077
rafael <rafael_sousa2018@outlook.com>
2024-05-04 08:45:10 -0300

    show message if dont have lines of the table based in the resource

--- resources/views/components/table/tbody.blade.php
+++ resources/views/components/table/tbody.blade.php
@@ -1,3 +1,12 @@
+@props(['lines' => null])
+
 <tbody {{ $attributes->merge(['class' => 'bg-gray-700 divide-y divide-gray-600']) }}>
+
+    @if ($lines === 0)
+        <x-table.tr>
+            <x-table.td colspan="4" class="text-center">{{ __('Nenhum registro encontrado') }}</x-table.td>
+        </x-table.tr>
+    @endif
+
     {{ $slot }}
 </tbody>
