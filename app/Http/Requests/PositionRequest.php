164232054e67bf1049100434bd4482247bdf400f
rafael <rafael_sousa2018@outlook.com>
2024-05-04 18:22:11 -0300

    update position request

--- app/Http/Requests/PositionRequest.php
+++ app/Http/Requests/PositionRequest.php
@@ -2,45 +2,37 @@
 
 namespace App\Http\Requests;
 
-use App\Models\Position;
 use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class PositionRequest extends FormRequest
 {
-    protected Position $position;
-
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
+        $positionId = $this->route('position')->id;
+
         return [
             'shortName' => [
                 'required',
                 'min:3',
                 'max:10',
-                Rule::unique('positions', 'shortName')
-                    ->ignore($this->position)
+                'string',
+                Rule::unique('positions', 'shortName')->ignore($positionId)
             ],
             'name' => [
                 'required',
                 'min:3',
                 'max:50',
-                Rule::unique('positions', 'name')
-                    ->ignore($this->position)
+                'string',
+                Rule::unique('positions', 'name')->ignore($positionId)
             ],
-            'priority' => ['required', 'integer']
+            'priority' => [
+                'required',
+                'integer',
+                'min:1',
+                'max:999',
+                Rule::unique('positions', 'priority')->ignore($positionId)
+            ]
         ];
     }
 }
