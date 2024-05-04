6c8d9b1729adfc35ab2128501d0aeddd524b172d
rafael <rafael_sousa2018@outlook.com>
2024-05-04 18:21:49 -0300

    fix to get all validation errors (debug and table uses)

--- resources/views/components/execution-message.blade.php
+++ resources/views/components/execution-message.blade.php
@@ -1,7 +1,7 @@
 <div class="flex justify-left my-2">
-    @if ($errors->onValidation->isNotEmpty())
+    @if ($errors->any())
         <div class="bg-red-800 text-white py-2 px-4 rounded">
-            <span class="block">{{ implode('|', $errors->onValidation->all()) }}</span>
+            <span class="block">{{ implode('<br>', $errors->all()) }}</span>
         </div>
     @elseif(session('success_message'))
         <div class="bg-green-700 text-white py-2 px-4 rounded">
