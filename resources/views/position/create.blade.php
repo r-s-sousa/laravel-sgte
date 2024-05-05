e85d1168596712e8990cdf06d30c81651b52bd14
rafael <rafael_sousa2018@outlook.com>
2024-05-05 03:55:12 -0300

    finish position controller

--- resources/views/position/create.blade.php
+++ resources/views/position/create.blade.php
@@ -1,3 +1,49 @@
-<div>
-    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
-</div>
+<x-app-layout>
+    <x-slot name="title">Criar | Graduação</x-slot>
+    <x-body-layout>
+        <section class="sm:py-4 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <header>
+                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                    {{ __('Graduação') }}
+                </h2>
+
+                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                    {{ __('Atualize as informações da graduação.') }}
+                </p>
+            </header>
+
+            <form method="post" action="{{ route('position.store') }}" class="mt-6 space-y-6">
+                @csrf
+
+                <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                    <div>
+                        <x-input-label for="shortName" :value="__('Abreviação')" />
+                        <x-text-input id="shortName" name="shortName" type="text" class="mt-1 block w-full"
+                            :value="old('shortName')" required autofocus autocomplete="shortName" />
+                        <x-input-error class="mt-2" :messages="$errors->get('shortName')" />
+                    </div>
+
+                    <div>
+                        <x-input-label for="name" :value="__('Nome')" />
+                        <x-text-input id="name" name="name" type="text" class="mt-1 block w-full"
+                            :value="old('name')" required autocomplete="name" />
+                        <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                    </div>
+
+                    <div>
+                        <x-input-label for="priority" :value="__('Prioridade')" />
+                        <x-text-input id="priority" name="priority" type="number" class="mt-1 block w-full"
+                            :value="old('priority')" required autofocus autocomplete="priority" />
+                        <x-input-error class="mt-2" :messages="$errors->get('priority')" />
+                    </div>
+                </div>
+
+                <div class="flex items-center mt-6">
+                    <x-secondary-link-button class="mr-4"
+                        href="{{ route('position.index') }}">{{ __('Listar') }}</x-secondary-link-button>
+                    <x-primary-button>{{ __('Salvar') }}</x-primary-button>
+                </div>
+            </form>
+        </section>
+    </x-body-layout>
+</x-app-layout>
