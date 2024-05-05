5172e9947fdfe7e5e8924f9f472547cfb84f07c9
rafael <rafael_sousa2018@outlook.com>
2024-05-05 09:05:03 -0300

    changes files to feature automatized tests

--- resources/views/components/execution-message.blade.php
+++ resources/views/components/execution-message.blade.php
@@ -1,10 +1,13 @@
-@if ($errors->any())
-    <div class="flex justify-left my-2">
-        <div class="bg-red-800 text-white py-2 px-4 rounded">
-            <span class="block">{{ implode('<br>', $errors->all()) }}</span>
+@if (isset($errors))
+    @if ($errors->any())
+        <div class="flex justify-left my-2">
+            <div class="bg-red-800 text-white py-2 px-4 rounded">
+                <span class="block">{{ implode('<br>', $errors->all()) }}</span>
+            </div>
         </div>
-    </div>
-@elseif(session('success_message'))
+    @endif
+@endif
+@if (session('success_message'))
     <div class="flex justify-left my-2">
         <div class="bg-green-700 text-white py-2 px-4 rounded">
             <span class="block">{{ session('success_message') }}</span>
