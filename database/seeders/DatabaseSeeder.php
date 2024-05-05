55b79fb6c99187c821fffcd706236a50b5dd948e
rafael <rafael_sousa2018@outlook.com>
2024-05-05 09:40:58 -0300

    run all seeders in one time command

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,15 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => bcrypt('password'),
+        $this->call([
+            UserSeeder::class,
+            PositionSeeder::class
         ]);
     }
 }
