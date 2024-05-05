a5241ac045463e51d065f4415f884e7642ae0645
rafael <rafael_sousa2018@outlook.com>
2024-05-05 01:42:14 -0300

    margin y

--- resources/views/components/table/search.blade.php
+++ resources/views/components/table/search.blade.php
@@ -1,6 +1,6 @@
 @props(['search' => null, 'routeName' => null])
 
-<div {{ $attributes->merge(['class' => 'flex justify-end mt-4']) }}>
+<div {{ $attributes->merge(['class' => 'flex justify-end my-4']) }}>
     <form action="{{ route($routeName) }}" method="GET" class="flex items-center">
         <input type="text" name="search" placeholder="Search..." value="{{ $search ?? '' }}"
             class="px-4 py-2 mr-1 rounded-md focus:outline-none focus:ring focus:border-blue-300 bg-gray-800 text-gray-300 placeholder-gray-500 w-72 sm:w-auto">
